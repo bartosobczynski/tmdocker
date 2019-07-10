8bb29da6bfc98497230e424226aefd4c801e31a2
Bartosz Sobczynski <bartosz.sobczynski@netguru.pl>
2019-07-10 20:18:43 +0200

    Remove spec

--- cypress/integration/visit_homepage.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Visit homepage', function() {
-    it('Contains "Yay! You’re on Rails!"', function() {
-        cy.visit('http://0.0.0.0:3900')
-        cy.get('h1').contains('Yay! You’re on Rails!')
-    })
-})
